5ef2207b14a5e9c6944e1a36740cc5c23ccf1030
lili <leila.karkhi@gmail.com>
2018-07-09 00:05:36 +0430

    Project structure and config htaccess file

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reza
+ * Date: 08/07/2018
+ * Time: 09:59 PM
+ */
+
+class IndexController
+{
+
+}
---
+++ app/models/Users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reza
+ * Date: 08/07/2018
+ * Time: 10:00 PM
+ */
+
+class Users
+{
+
+}
