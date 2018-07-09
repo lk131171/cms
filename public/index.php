37af777b2f171aac384425ffa2105e26a5f90421
lili <leila.karkhi@gmail.com>
2018-07-09 13:11:32 +0430

    Register autoloaders

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "hi";
+use phalcon\Loader;
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__). '/');
+define('APP_PATH', BASE_PATH. 'app/');
+// Register AutoLoaders
+$loader = new Loader();
+$loader->registerDirs(
+    [
+        APP_PATH. 'controllers/',
+        APP_PATH. 'models/',
+    ]
+)->register();
+
